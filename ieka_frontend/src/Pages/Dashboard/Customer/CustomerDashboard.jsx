5834c3b6252049aa59082e359d54ca4fbb5599c7
JESSE OGBONNA <jessesamuel84@gmail.com>
2023-02-03 22:00:26 +0200

    fix: adds chats link to sidebar

--- ieka_frontend/src/Pages/Dashboard/Customer/CustomerDashboard.jsx
+++ ieka_frontend/src/Pages/Dashboard/Customer/CustomerDashboard.jsx
@@ -10,7 +10,7 @@ const CustomerDashboard = () => {
 	const [searchTerm, setSearchTerm] = useState('');
 
 	return (
-		<div className='flex bg-gray-100 flex-col md:flex-row placeholder:h-screen transition-height duration-75 ease-out'>
+		<div className='flex bg-gray-100 text-gray-700 flex-col md:flex-row placeholder:h-screen transition-height duration-75 ease-out'>
 			<DashboardLayout sidebarLinks={CustomerSideBarLinks} />
 			<div className='pb-2 flex-1 h-screen overflow-y-scroll hide-scrollbar'>
 				<DashboardNavBar
